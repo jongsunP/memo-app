3ebeb75fa5c404e51908ee6c24e12ea5b2101660
jongsunP <jongsun@dentlink.app>
2025-04-13 18:59:06 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  assetPrefix: "https://jongsunP.github.io/memo-app",
 };
 
 export default nextConfig;
