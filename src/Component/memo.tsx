1c3f820ea63acea920150e9723a61c738dbfc68a
jongsunP <jongsun@dentlink.app>
2025-04-13 20:46:14 +0900

    test localStorage

--- src/Component/memo.tsx
+++ src/Component/memo.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./memo.module.css";
 
 const Memo = () => {
-  return <div className={styles.memoContainer}>Memo</div>;
+  const [memo, setMemo] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMemo(e.target.value);
+  };
+
+  const handleSave = () => {
+    localStorage.setItem("memo", memo);
+  };
+
+  useEffect(() => {
+    const memo = localStorage.getItem("memo");
+    if (memo) {
+      setMemo(memo);
+    }
+  }, []);
+
+  return (
+    <div className={styles.memoContainer}>
+      Memo
+      <textarea
+        className={styles.textarea}
+        onChange={handleChange}
+        value={memo}
+      />
+      <button className={styles.button} onClick={handleSave}>
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default Memo;
